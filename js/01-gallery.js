25bddbc30ff8b1497fd68a1c7374ad540414688a
IevgenLychman <gora.erikovich@gmail.com>
2022-11-06 18:13:38 +0200

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createImageEl(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>`;
+    })
+    .join("");
+}
+
+const galleryMarkupTemplate = createImageEl(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkupTemplate);
+
+galleryContainer.addEventListener("click", selectImg);
+
+function selectImg(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <img src="${e.target.dataset.source}" width="800" height="600"> `);
+
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
