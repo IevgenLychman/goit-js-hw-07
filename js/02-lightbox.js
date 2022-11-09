4544c488e00736b5768e46fde02c0a0a1d920451
IevgenLychman <gora.erikovich@gmail.com>
2022-11-09 13:18:48 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+
+function createImageEl(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li>
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      </li>`;
+    })
+    .join("");
+}
+
+const galleryMarkupTemplate = createImageEl(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkupTemplate);
+
+const gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  `${description}`;
+});
